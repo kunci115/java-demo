7aa0c4420794a826c110c3b8ca5b28c955585188
kunci115 <rino.alpin@gmail.com>
2020-10-19 05:29:42 +0700

    hello world get

--- src/main/java/com/example/demo/DemoSpringWebApplication.java
+++ src/main/java/com/example/demo/DemoSpringWebApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/hello")
 public class DemoSpringWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringWebApplication.class, args);
 	}
-
+	@GetMapping
+	public String sayHello(){
+		return "Hello World";
+}
 }
