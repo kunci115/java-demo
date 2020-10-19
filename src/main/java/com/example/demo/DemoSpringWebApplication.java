c7ca465f6e8cc32ed3d88e2e4e45d9573b671b59
kunci115 <rino.alpin@gmail.com>
2020-10-19 16:12:55 +0700

    controller

--- src/main/java/com/example/demo/DemoSpringWebApplication.java
+++ src/main/java/com/example/demo/DemoSpringWebApplication.java
@@ -7,15 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/hello")
 public class DemoSpringWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringWebApplication.class, args);
 	}
-	@GetMapping
-	public String sayHello(){
-		return "Hello World";
-}
 }
